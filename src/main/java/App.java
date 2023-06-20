2587f3e99297f10f72b714d7838f1691469d483c
shiga <hikikomorim@gmail.com>
2023-06-20 14:43:57 +0900

    2.1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -8,5 +9,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == bean2);
+        System.out.println(bean);
+        System.out.println(bean2);
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean.getMessage());
+        Cat bean2Cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getMessage());
+        System.out.println(beanCat == bean2Cat);
     }
 }
